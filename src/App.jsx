a1eb7b641634ff6f60a4d4ba4fb6ce0c79a86881
Anas Asif <154005474+DesignByAnas@users.noreply.github.com>
2024-09-06 06:55:21 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Card } from './components/Card'
 
 function App() {
   return (
     <>
     <h1>Hello World</h1>
-    <Card>
+    <Card></Card>
     </>
   )
 }
